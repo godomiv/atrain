0ded676778e5af83a14fa7355ebe74b4e07f0e8d
Ivan Godomin <godomiv@gmail.com>
2025-06-27 14:35:13 +0400

    Create __init__.py

---
+++ core/batch/__init__.py
@@ -0,0 +1,12 @@
+# atrain/core/batch/__init__.py
+"""
+Система batch операций
+"""
+
+from .batch_processor import BatchProcessor
+from .batch_operations import BatchOperations
+
+__all__ = [
+    'BatchProcessor',
+    'BatchOperations'
+]
